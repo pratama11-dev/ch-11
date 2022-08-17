d438a94b99fa1aee08b628839262ea865351a408
wolhaiksongmazino <alrizal.luciel@gmail.com>
2022-08-17 12:02:47 +0700

    koreksi app.js terdelete

---
+++ pages/_app.jsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+// add bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default
